0dde8cbb2775efb2333834d170280e04d4252382
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2023-10-03 11:45:40 +0700

    initial commit

---
+++ MarketplaceNameDetection/MarketplaceNameDetection/MarketplaceNameDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceNameDetectionApp.swift
+//  MarketplaceNameDetection
+//
+//  Created by Achmad Rijalu on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MarketplaceNameDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
